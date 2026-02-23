60a54b56da8df464b6e8370080019ea8add9359d
onelight-levi <onelight-levi@github.com>
2026-02-23 02:51:46 +0000

    Update to real web server

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(port, () => {
+    console.log("Express 웹 서버가 ${port}번 포트에서 가동 중입니다...");
+});
